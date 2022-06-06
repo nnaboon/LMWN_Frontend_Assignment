61f3b764d44b7e89e5ea3aeb3d11d9246e391446
nnaboon <srisawasdina@gmail.com>
2022-06-06 10:47:19 +0700

    feat: theme text color

---
+++ web/src/utils/theme.ts
@@ -0,0 +1,14 @@
+export const isGradientColor = (color: string) => {
+  return color?.includes("linear-gradient");
+};
+
+export const TEXT_COLOR = {
+  primary: "#2C9BDB",
+  secondary: "#929292",
+  search: "#BDBDBD",
+};
+
+export const BORDER_RADIUS = {
+  card: "10px",
+  button: "4px",
+};
