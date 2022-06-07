31e09cc334c6a5a23c69c47b7993caccd0c28efe
nnaboon <srisawasdina@gmail.com>
2022-06-07 11:11:52 +0700

    fix: change react-router-dom using component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,17 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { css, jsx } from "@emotion/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { jsx } from "@emotion/react";
 import { LandingPage } from "./pages/Landing";
 
 function App() {
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <Switch>
-          <Route path='/' exact component={LandingPage} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='*' element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
