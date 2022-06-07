efcf61b7ed97a6b838993e6eba83676219e87b72
nnaboon <srisawasdina@gmail.com>
2022-06-07 11:11:10 +0700

    fix: config API path

--- web/src/hooks/useTrips.ts
+++ web/src/hooks/useTrips.ts
@@ -1,19 +1,18 @@
 import useAxios from "axios-hooks";
+import { API_URL } from "config";
 
 export const useTrips = () => {
   const [{ data: response, loading, error }, fire] = useAxios(
     {
       method: "GET",
-      headers: { "Access-Control-Allow-Origin": "*" },
     },
     { manual: true }
   );
 
   const execute = (keyword?: string) => {
     return fire({
-      url: `http://localhost:3001/api/trips`,
+      url: `${API_URL}/api/trips`,
       params: { keyword: keyword },
-      headers: { "Access-Control-Allow-Origin": "*" },
     });
   };
 
